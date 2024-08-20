a8708630607e0c2e542ffc48c8bbd5be4538b737
Iwan de Jong <idj.idejong@gmail.com>
2024-08-20 15:18:10 +0200

    added documentation

--- interfaces/Employee.ts
+++ interfaces/Employee.ts
@@ -1,26 +1,3 @@
-// CREATE TABLE [dbo].[employee] (
-//     [id]          INT                 IDENTITY (1, 1) NOT NULL,
-//     [firstname]   NVARCHAR (50)       NULL,
-//     [lastname]    NVARCHAR (50)       NULL,
-//     [birthdate]   DATE                NULL,
-//     [employeeid]  UNIQUEIDENTIFIER    NULL,
-//     [salary]      FLOAT (53)          NULL,
-//     [role]        NVARCHAR (50)       NULL,
-//     [manager]     UNIQUEIDENTIFIER    NULL,
-//     [joiningdate] DATE                NULL,
-//     [leavedays]   INT                 NULL,
-//     [linkedin]    VARCHAR (MAX)       NULL,
-//     [orgid]       UNIQUEIDENTIFIER    NULL,
-//     [email]       VARCHAR (MAX)       NULL,
-//     [password]    VARCHAR (MAX)       NULL,
-//     [hierarchyId] [sys].[hierarchyid] NULL,
-//     [bio]         TEXT                NULL,
-//     [gravatarurl] NVARCHAR (MAX)      NULL,
-//     CONSTRAINT [PK_emp] PRIMARY KEY CLUSTERED ([id] ASC),
-//     CONSTRAINT [FK_org] FOREIGN KEY ([orgid]) REFERENCES [dbo].[organisation] ([id]) ON DELETE CASCADE ON UPDATE CASCADE,
-//     INDEX [GRAPH_UNIQUE_INDEX_60C23A527BEC49ABB7E8AFC627A748CB] UNIQUE NONCLUSTERED ($node_id)
-// ) AS NODE;
-
 interface DefineEmployee {
     id: number;
     firstname: string;
