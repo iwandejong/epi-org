9d7f233bc74d3e3bcc27ce4be87910b429a822ce
Iwan de Jong <idj.idejong@gmail.com>
2024-08-20 11:52:53 +0200

    migrated from pg to @vercel/postgres

--- server/api/db/connection.ts
+++ server/api/db/connection.ts
@@ -1,16 +1,12 @@
-import pg from 'pg';
-const { Pool } = pg;
+import { createClient } from '@vercel/postgres';
 
-const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-    ssl: process.env.POSTGRES_URL?.includes('sslmode=require') || process.env.POSTGRES_URL_NO_SSL?.includes('sslmode=require')
-        ? { rejectUnauthorized: false }
-        : false
+const client = createClient({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
-export const poolPromise = pool.connect();
-export default pool;    
+export const poolPromise = client.connect();
+
+export default client;
